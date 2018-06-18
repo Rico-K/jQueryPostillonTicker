6ade532325bcba4a212d63385005e5038cfb08a2
Rico-K <ricokritz@web.de>
2018-06-18 15:37:42 +0200

    - added examples and jQuery plugin - updated README

--- README.md
+++ README.md
@@ -1,1 +1,255 @@
-# jQueryPostillonTicker
+jQueryPostillonTicker
+=====================
+
+Table of contents
+-----------------
+
+1. [About](#about)
+2. [Requirements](#requirements)
+3. [Usage](#usage)
+4. [Settings](#settings)
+    1. [Set on initialization](#set-on-initialization)
+    2. [Set after initialization](#set-after-initialization)
+        1. [Set single setting](#set-single-setting)
+        2. [Set multiple settings](#set-multiple-settings)
+        3. [Set all settings](#set-all-settings)
+    3. [Change defaults](#change-defaults)
+5. [Functions](#functions)
+    1. [Calling](#calling)
+6. [Events](#events)
+7. [Examples](#examples)
+    1. [Simple ticker](#simple-ticker)
+    2. [Pause on mouse over](#pause-on-mouse-over)
+    3. [Updating manually](#updating-manually)
+    4. [No fade](#no-fade)
+
+About
+-----
+
+This is a little jQuery plugin that loads the postillon news tickers and displays them in a HTML
+container of your choice updating it automatically.
+
+Check out some of the world's most relevant news on
+<a href="https://www.der-postillon.com/" target="_blank">Postillon</a>
+
+Requirements
+------------
+
+* jQuery
+* sense of humor
+
+Usage
+-----
+
+    <html>
+        <head>
+            <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+            <script type="text/javascript" src="jquery.postillonTicker.js"></script>
+            <script type="text/javascript">
+            //<!--
+                $(function() {
+                    $("#ticker").postillonTicker({
+                        autoStart: true,
+                        format: "+++ %s +++",
+                        instantStart: true,
+                        interval: 10000,
+                        showLinks: true,
+                    });                    
+                });
+            //-->
+            </script>
+        </head>
+        <body>
+	        <div id="ticker"></div>
+	    </body>
+    </html>
+
+Settings
+--------
+
+* **autoStart** (*boolean*) - starts automatic ticker updating after initializing the plugin
+(default: `true`)
+* **fadeInTime** (*integer*) - durance of the fade in animation in milliseconds (default: 1000)
+* **fadeOutTime** (*integer*) - durance of the fade out animation in milliseconds (default: 1000)
+* **format** (*string*) - format of the shown ticker text that may contain printf-like string
+placeholders (`%s`) where the first placeholder is replaced with the ticker text and the second one
+with the short name of the ticker's writer (default: `+++ %s +++ (%s)`)
+* **instantStart** (*boolean*) - instantly starts the ticker updater after initialization without
+waiting one interval (default: `true`)
+* **interval** (*integer*) - updating interval in milliseconds (default: `15000`)
+* **showLinks** (*boolean*) - shows the text as link to the ticker's article if it has one (default:
+`true`)
+* **tickerUrl** (*string*) - URL of the postillon ticker API (default:
+`https://www.der-postillion.de/ticker/newsticker2.php`)
+
+### Set on initialization
+
+The easiest way to set settings for one ticker is by passing an Object to the plugin's constructor:
+
+    $("#ticker").postillonTicker({
+        format: "+++ %s +++",
+        interval: 10000,
+        showLinks: false,
+    });
+
+### Set after initialization
+
+You can also set settings after the plugin has be initialized in three different ways:
+
+#### Set single setting
+
+    $("#ticker").postillonTicker("setSetting", "interval", "5000");
+    
+#### Set multiple settings:
+
+    $("#ticker").postillonTicker("mergeSettings", {
+        format: "+++ %s +++",
+        interval: 5000,
+    });
+
+#### Set all settings:
+
+You will almost never need this function except maybe for something like restoring previously saved
+settings or something like this since this function will wipe out all settings not explicitly set.
+
+**Beware:** Using this function may also cause unexpected behavior since you may delete some
+"private" settings like the fetched tickers, the current ticker index or the interval index.
+
+    $("#ticker").postillonTicker("setSettings", {
+        autoStart: true,
+        fadeInTime: 1000,
+        fadeOutTime: 1000,
+        format: "+++ %s +++ (%s)",
+        instantStart: true,
+        interval: 10000,
+        showLinks: true,
+        tickerUrl: "https://www.der-postillion.de/ticker/newsticker2.php",
+        _paused: false,
+        _tickerIdx: 0,
+        _tickers: [],
+        _timer: null,
+    });
+    
+You'd rather use this function in that kind of way:
+
+    var settings = $("#ticker").postillonTicker("getSettings");
+    
+    settings.interval = 5000;
+    
+    $("#ticker").postillonTicker("setSettings", settings);
+    
+This is also the way the `setSetting` and `mergeSettings` functions work internally. However, you
+should prefer `setSetting` or `mergeSettings` instead of `setSettings`.
+
+### Change defaults
+
+Beside changing the settings on or after initialization you can also change the default settings of
+the plugin which will effect all future plugin constructor calls:
+
+Check this out:
+
+    $("#ticker1").postillonTicker(); // has default interval of 15 seconds
+    $("#ticker1").postillonTicker("setSetting", "interval", 10000); // has now 10 seconds interval
+    $("#ticker2").postillonTicker(); // ticker nr. 2 has also default of 15 seconds
+
+Changing defaults:
+
+    // all new tickers will have an interval of 5 seconds instead of 15 seconds
+    $.fn.postillonTicker.defaults.interval = 5000;
+    
+    $("#ticker1").postillonTicker(); // 5 seconds interval
+    $("#ticker2").postillonTicker(); // also 5 seconds interval
+
+Functions
+---------
+
+* **getCurrentTicker()** - returns the current ticker object returned by the ticker API
+* **getSetting(name)** - returns the value of the setting with the given name
+* **getSettings()** - returns an Object containing all settings
+* **isRunning()** - returns whether the updater is running
+* **mergeSettings(settings)** - merges the given settings into the existing settings
+* **pause()** pauses the updater
+* **restart()** - restarts the updater calling stop and start
+* **resume()** - resumes the updater when it previously has been paused
+* **setSetting(name, value)** - sets the setting with the given name to the given value
+* **setSettings(settings)** - sets the whole settings object
+* **start()** - starts the updater
+* **stop()** - stops the updater
+* **update([callback])** - updates the ticker container loading a new ticker text
+
+### Calling
+
+The functions have to be called in the following way:
+
+    $("#ticker").postillonTicker("methodName", argument1, argument2);
+    
+Example:
+
+    // sets the updating interval to 20 seconds
+    $("#ticker").postillonTicker("setSetting", "interval", 20000);
+
+Events
+------
+
+* **intervalChanged.postillonTicker** - triggered when the interval has been changed (**note**:
+this is an internal event listener on the *settingsChanged.postillonTicker* event, so calling
+something like `$("#ticker").off("settingsChanged.postillonTicker")`) will prevent the
+*intervalChanged.postillonTicker* event from being triggered)
+* **paused.postillonTicker** - triggered when the updater has been paused
+* **resumed.postillonTicker** - triggered when the updater has been resumed
+* **settingsChanged.postillonTicker** - triggered when settings have changed
+* **started.postillonTicker** - triggered when the updater has been started
+* **starting.postillonTicker** - triggered when the updater is starting
+* **stopped.postillonTicker** - triggered when the updater has been stopped
+* **stopping.postillonTicker** - triggered when the updater is stopping
+* **updated.postillonTicker** - triggered when the updater finished updating
+* **updating.postillonTicker** - triggered when the updater is updating
+
+Examples
+--------
+
+You can see some live examples in the examples folder. Here are the short versions of those examples
+reduced to the JavaScript part:
+
+### Simple ticker
+
+    $("#ticker").postillonTicker({
+        autoStart: true,
+        format: "+++ %s +++",
+        instantStart: true,
+        interval: 10000,
+        showLinks: true,
+    });
+
+### Pause on mouse over
+
+    $("#ticker").postillonTicker({
+        format: "+++ %s +++",
+        interval: 3000,
+    });
+    $("#ticker").on("mouseenter", function() {
+        $("#ticker").postillonTicker("pause");
+    });
+    $("#ticker").on("mouseleave", function() {
+        $("#ticker").postillonTicker("resume");
+    });
+    
+### Updating manually
+
+    $("#ticker").postillonTicker({
+        autoStart: false,
+        format: "+++ %s +++",
+    });
+    $("#update_button").on("click", function () {
+        $("#ticker").postillonTicker("update");
+    });
+
+### No fade
+
+    $("#ticker").postillonTicker({
+        autoStart: true,
+        fadeInTime: 0,
+        fadeOutTime: 0,
+        format: "+++ %s +++",
+        interval: 5000,
+    });
---
+++ jquery.postillonTicker.js
@@ -0,0 +1,381 @@
+/**
+ * @author Rico-K
+ *
+ * <b>Settings:</b>
+ * <ul>
+ *   <li>autoStart - updating starts after initialization (default: true)
+ *   <li>fadeInTime - durance of the fade in animation in milliseconds (default: 1000)
+ *   <li>fadeOutTime - durance of the fade out animation in milliseconds (default: 1000)
+ *   <li>format - format of the ticker that gets the text and the short name of the writer passed as
+ *                parameters used as printf like string (%s) (default: "+++ %s +++ (%s)")
+ *   <li>instantStart - loads ticker directly after start without waiting the first interval
+ *                      (default: true)
+ *   <li>interval - interval of updating in milliseconds (default: 15000)
+ *   <li>showLinks - defines whether to show tickers with links as links or not (default: true)
+ *   <li>tickerUrl - url of postillon ticker API
+ * </ul>
+ *
+ * <b>Methods:</b>
+ * <ul>
+ *   <li>getCurrentTicker - returns the current ticker object
+ *   <li>getSetting - returns the setting with the given name or null
+ *   <li>getSettings - returns the settings of the plugin
+ *   <li>isRunning - returns whether the ticket updater is running
+ *   <li>mergeSettings - merges the given settings into the existing settings
+ *   <li>pause - pauses the updater
+ *   <li>resume - resumes the updater when previoulsy paused
+ *   <li>restart - restarts the ticker updater
+ *   <li>setSetting - sets the given setting (expects name and value of setting)
+ *   <li>setSettings - sets the givven settings as object
+ *   <li>start - starts the ticker updater
+ *   <li>stop> - stops the ticker updater
+ *   <li>update - updates the ticker container by showing a new ticker
+ * </ul>
+ *
+ * <b>Events:</b>
+ * <ul>
+ *   <li>intervalChanged.postillonTicker - triggered when the interval has changed
+ *   <li>paused.postillonTicker - triggered when the updater gets paused
+ *   <li>resumed.postillonTicker - triggered when the updater gets resumed
+ *   <li>settingsChanged.postillonTicker - triggered when settings have changed
+ *   <li>started.postillonTicker - triggered after the updater has started
+ *   <li>starting.postillonTicker - triggered when the updater is starting
+ *   <li>stopped.postillonTicker - triggered after the updater has stopped
+ *   <li>stopping.postillonTicker - triggered when the updater is stopping
+ *   <li>updated.postillonTicker - triggered after the updater has updated
+ *   <li>updating.postillonTicker - triggered when the updater is updating
+ * </ul>
+ */
+(function ($, w) {
+    "use strict";
+
+    var NS = "postillonTicker";
+    var methods = {
+        /********************/
+        /* public functions */
+        /********************/
+
+        /**
+         * Returns the current ticker object.
+         * @returns {Object}
+         */
+        getCurrentTicker: function () {
+            var tickers     = methods.getSetting.call(this, "_tickers");
+            var tickerIdx   = methods.getSetting.call(this, "_tickerIdx");
+
+            return tickers[tickerIdx];
+        },
+
+        /**
+         * Checks whether the updater is running.
+         * @returns {bool}
+         */
+        isRunning: function() {
+            var timer = methods._getTimer.call(this);
+            return timer ? true : false;
+        },
+
+        /**
+         * Pauses the updater.
+         * @returns {jQuery}
+         */
+        pause: function() {
+            var jObj = $(this);
+            if (!methods.getSetting.call(this, "_paused"))
+                jObj.trigger($.Event("paused." + NS, {}));
+
+            methods.setSetting.call(this, "_paused", true);
+
+            return this;
+        },
+
+        /**
+         * Restarts the updater.
+         * @returns {jQuery}
+         */
+        restart: function() {
+            methods.stop.call(this);
+            methods.start.call(this);
+            return this;
+        },
+
+        /**
+         * Resumes the updater if it has been paused previously.
+         * @returns {jQuery}
+         */
+        resume: function () {
+            var jObj = $(this);
+            if (methods.getSetting.call(this, "_paused"))
+                jObj.trigger($.Event("resumed." + NS, {}));
+
+            methods.setSetting.call(this, "_paused", false);
+
+            return this;
+        },
+
+        /**
+         * Starts the updater.
+         * @returns {jQuery}
+         */
+        start: function() {
+            var timer = methods._getTimer.call(this);
+            if (timer) return this;
+
+            var jObj = $(this);
+
+            jObj.trigger($.Event("starting." + NS, {}));
+
+            if (methods.getSetting.call(this, "instantStart"))
+                methods.update.call(this);
+
+            methods._setTimer.call(this, w.setInterval(function() {
+                if (methods.getSetting.call(jObj, "_paused")) return;
+                methods.update.call(jObj);
+            }, methods.getSetting.call(this, "interval")));
+
+            jObj.trigger($.Event("started." + NS, {}));
+
+            return this;
+        },
+
+        /**
+         * Stops the updater.
+         * @returns {jQuery}
+         */
+        stop: function() {
+            var timer = methods._getTimer.call(this);
+            if (!timer) return this;
+
+            var jObj = $(this);
+
+            jObj.trigger($.Event("stopping." + NS, {}));
+
+            w.clearInterval(timer);
+
+            methods._setTimer.call(this, null);
+
+            jObj.trigger($.Event("stopped." + NS, {}));
+
+            return this
+        },
+
+        /**
+         * Loads the next ticker and updates the container.
+         * @returns {jQuery}
+         */
+        update: function() {
+            var jObj = $(this);
+            jObj.trigger($.Event("updating." + NS, {}));
+
+            var tickers     = methods.getSetting.call(this, "_tickers");
+            var tickerIdx   = methods.getSetting.call(this, "_tickerIdx");
+            var newIdx      = tickerIdx + 1;
+
+            var update = function () {
+                var ticker = methods.getCurrentTicker.call(jObj);
+                jObj.fadeOut(methods.getSetting.call(jObj, "fadeOutTime"), function() {
+                    var format      = methods.getSetting.call(jObj, "format");
+                    var text        = ticker.text;
+                    if ("link" in ticker && methods.getSetting.call(jObj, "showLinks"))
+                        text = "<a href=\"" + ticker.link + "\" target=\"_blank\">" + text + "</a>";
+
+                    text = format.replace("%s", text);
+                    text = text.replace("%s", ticker.short);
+
+                    jObj.html(text);
+                    jObj.fadeIn(methods.getSetting.call(jObj, "fadeInTime"), function() {
+                        jObj.trigger($.Event("updated." + NS, {ticker: ticker}));
+                    });
+                });
+            };
+
+            if (newIdx < tickers.length) {
+                methods.setSetting.call(jObj, "_tickerIdx", newIdx);
+                update.call(this);
+            }
+            else {
+                methods._loadTickers.call(this, update);
+            }
+
+            return this;
+        },
+
+        /*********************/
+        /* setting functions */
+        /*********************/
+
+        /**
+         * Returns the setting with the given name or null if it does not exist.
+         * @param {string} name
+         * @return {mixed}
+         */
+        getSetting: function (name) {
+            var settings = methods.getSettings.call(this);
+            return name in settings ? settings[name] : null;
+        },
+
+        /**
+         * Returns the settings of the plugin.
+         * @return Object
+         */
+        getSettings: function () {
+            return this.data(NS + ".settings");
+        },
+
+        /**
+         * Merges the given settings into the existing settings.
+         * @param settings
+         * @return jQuery
+         */
+        mergeSettings: function (settings) {
+            var existing = methods.getSettings.call(this);
+            methods.setSettings.call(this, $.extend(true, existing, settings));
+
+            return this;
+        },
+
+        /**
+         * Sets the given setting.
+         * @param setting
+         * @param value
+         * @return jQuery
+         */
+        setSetting: function (setting, value) {
+            var settings = $.extend({}, methods.getSettings.call(this));
+            settings[setting] = value;
+            methods.setSettings.call(this, settings);
+
+            return this;
+        },
+
+        /**
+         * Sets the settings for the plugin.
+         * @param settings
+         * @return jQuery
+         */
+        setSettings: function (settings) {
+            var old = methods.getSettings.call(this);
+            this.data(NS + ".settings", settings);
+            this.trigger($.Event("settingsChanged." + NS, {old: old, "new": settings}));
+
+            return this;
+        },
+
+        /*********************/
+        /* private functions */
+        /*********************/
+
+        /**
+         * Returns the timer.
+         * @returns {number}
+         * @private
+         */
+        _getTimer: function() {
+            return methods.getSetting.call(this, "_timer");
+        },
+
+        /**
+         * Initializes the postillonTicker.
+         * @param options
+         * @private
+         */
+        _init: function (options) {
+            var defaults = $.fn.postillonTicker.defaults;
+
+            return this.each(function () {
+                var jObj = $(this);
+                var settings = $.extend(true, {}, defaults, options);
+
+                var set = jObj.data(NS + ".settings");
+                if (set && set._initialized) {
+                    return;
+                }
+
+                // save settings in data
+                settings._initialized = true;
+                jObj.data(NS + ".settings", settings);
+
+                // register interval changed event
+                jObj.on("settingsChanged." + NS, function(e) {
+                    if (e.old.interval !== e.new.interval) {
+                        $(this).trigger($.Event("intervalChanged." + NS, {old: e.old.interval, "new": e.new.interval}));
+                    }
+                });
+                jObj.on("intervalChanged." + NS, function(e) {
+                    $(this).postillonTicker("restart");
+                });
+
+                if (settings.autoStart) methods.start.call(jObj);
+            });
+        },
+
+        /**
+         * Loads a new set of tickers.
+         * @param {callback} callback
+         * @returns {jQuery}
+         * @private
+         */
+        _loadTickers: function (callback) {
+            var pt = this;
+            $.get({
+                data: {},
+                dataType: "json",
+                success: function (data, status, jqXHR) {
+                    methods.setSetting.call(pt, "_tickers", data.tickers);
+                    methods.setSetting.call(pt, "_tickerIdx", 0);
+                    if (callback && typeof callback === "function")
+                        callback.call(pt);
+                },
+                url: methods.getSetting.call(this, "tickerUrl"),
+            });
+
+            return this;
+        },
+
+        /**
+         * Sets the timer.
+         * @private
+         */
+        _setTimer: function(timer) {
+            return methods.setSetting.call(this, "_timer", timer);
+        },
+    };
+
+    /**
+     * jQuery plugin constructor
+     * @param method
+     */
+    $.fn.postillonTicker = function (method) {
+        // constructor call with either no parameters or an object
+        if (method instanceof Object || !method) {
+            return methods._init.apply(this, arguments);
+        }
+        // public method call
+        else if (method in methods && !String(method).match(/^_/) && $.isFunction(methods[method])) {
+            var args = Array.prototype.slice.call(arguments, 1);
+            this.each(function () {
+                methods[method].apply($(this), args);
+            });
+        }
+        else {
+            $.error("There is no such method \"" + method + "\" in " + NS);
+        }
+    };
+
+    $.fn.postillonTicker.NS = NS;
+
+    $.fn.postillonTicker.defaults = {
+        autoStart: true,
+        fadeInTime: 1000,
+        fadeOutTime: 1000,
+        format: "+++ %s +++ (%s)",
+        instantStart: true,
+        interval: 15000,
+        showLinks: true,
+        tickerUrl: "https://www.der-postillion.de/ticker/newsticker2.php",
+        _paused: false,
+        _tickerIdx: 0,
+        _tickers: [],
+        _timer: null,
+    };
+})(jQuery, window);
